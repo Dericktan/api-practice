b70c0c74577eb8b251742e5d44b51051e80f67ef
Derick <derick.tan988@gmail.com>
2020-04-15 21:26:32 +0700

    Initial commit

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Blog;
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $blogs = Blog::all();
+        return response()->json($blogs);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $blog = new Blog;
+        $blog->title = $request->title;
+        $blog->content = $request->content;
+        $blog->save();
+
+        return response()->json($blog);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Blog  $blog
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Blog $blog)
+    {
+        return response()->json($blog);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Blog  $blog
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Blog $blog)
+    {
+        $blog->title = $request->title;
+        $blog->content = $request->content;
+        $blog->save();
+
+        return response()->json($blog);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Blog  $blog
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Blog $blog)
+    {
+        $blog->delete();
+        return response()->json($blog);
+    }
+}
